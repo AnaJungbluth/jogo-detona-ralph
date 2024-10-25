141ce2169bf9225789414165b19dfc8289f99d77
Ana Carolina Jungbluth <anajungbluth26@outlook.com>
2024-10-25 14:07:04 -0300

    segundo-commit

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,15 +2,35 @@ const state ={
     view:{
         squares: document.querySelectorAll(".square"),
         enemy: document.querySelector(".enemy"),
-        timeLeft: document.querySelector("#time-left"),
+        timeLeft: document.querySelector(".time-left"),
         score: document.querySelector("#score")
     },
     values:{
-        timerId: null,
-        gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
+        curretTime: 60,
     },
+    actions:{
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+}
+
+function countDown(){
+    state.values.curretTime--
+    state.view.timeLeft.textContent = state.values.curretTime
+
+    if(state.values.curretTime <= 0){
+        clearInterval(state.actions.countDownTimerId)
+        clearInterval(state.actions.timerId)
+        alert("Game Over! O seu resultado foi: " + state.values.result)
+    }
+}
+
+function playSound(audioName){
+    let audio = new Audio(`src/sons/${audioName}.m4a`)
+    audio.volume = 0.2
+    audio.play()
 }
 
 function randomSquare(){
@@ -24,10 +44,6 @@ function randomSquare(){
     state.values.hitPosition = randomSquare.id
 }
 
-function moveEnemy(){
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
-}
-
 function addListenerHitBox(){
     state.view.squares.forEach((square)=> {
         square.addEventListener("mousedown", ()=> {
@@ -35,13 +51,13 @@ function addListenerHitBox(){
                 state.values.result++
                 state.view.score.textContent = state.values.result
                 state.values.hitPosition = null
+                playSound("hit")
             }
         })
     })
 }
 
 function main(){
-    moveEnemy()
     addListenerHitBox()
 }
 
